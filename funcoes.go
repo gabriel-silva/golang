e615eccf97164fd30e360d91009fa9f2bde8914d
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-04-17 14:09:20 -0300

    modificando file funcoes

--- funcoes.go
+++ funcoes.go
@@ -12,6 +12,21 @@ func soma(x int, y int) int {
 	return x + y
 }
 
+//// 3 ex
+//func calcular(x int) (int, int) {
+//	var quadrado int =  x * x
+//	var cubo int = x * x * x
+//	return quadrado, cubo
+//}
+
+// 4 ex
+func calcular(x int) (quadrado int, cubo int) {
+	quadrado =  x * x
+	cubo = x * x * x
+	return
+}
+
 func main() {
 	fmt.Println("soma:", soma(3, 4))
+	fmt.Println(calcular(2))
 }
