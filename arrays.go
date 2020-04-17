26dfd724fca4a045dc825ab6d7485629d5c3c458
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-04-17 20:44:42 -0300

    adicionando file arrays

---
+++ arrays.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	//1 ex
+	//var numbers[5] int
+	//numbers[0] = 1
+	//numbers[1] = 2
+	//numbers[2] = 3
+	//numbers[3] = 4
+	//numbers[4] = 5
+
+	// 2 ex
+	var numbers = [5] int{1, 2, 3, 4, 5}
+	fmt.Println(numbers)
+}
