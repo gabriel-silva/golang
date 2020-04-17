7beab8df67e49e8d266b04aee66f094b346c35e7
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-04-17 07:32:36 -0300

    adicionando file de variaveis

---
+++ variaveis.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	// 1 ex
+	//var nome string = "Gabriel"
+
+	// 2 ex
+	//nome := "Gabriel"
+
+	// 3 ex
+	//var (
+	//	nome = "Gabriel"
+	//)
+
+	// 4 ex
+	var nome = "Gabriel"
+
+	fmt.Println("Meu nome é:", nome)
+}
