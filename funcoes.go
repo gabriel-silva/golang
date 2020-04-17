2d92c30d0ec862f74f24c994e5982a05b59f5893
Gabriel Neves <gabriel-silva_2011@hotmail.com>
2020-04-17 08:03:54 -0300

    adicionando file funcoes

---
+++ funcoes.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+// 1 ex
+//func soma(x, y int) int {
+//	return x + y
+//}
+
+// 2 ex
+func soma(x int, y int) int {
+	return x + y
+}
+
+func main() {
+	fmt.Println("soma:", soma(3, 4))
+}
